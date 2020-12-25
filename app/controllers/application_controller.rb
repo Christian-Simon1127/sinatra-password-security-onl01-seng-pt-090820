05bcef3acf82bea50ec211b5a73ff8618eb26851
Christian Simon <csimon1127@gmail.com>
2020-12-25 16:54:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,6 +32,12 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		user = User.find_by(username: params[:username])
+		
+		if user 
+		  redirect to "/success"
+		else 
+		  redirect to "/failure"
+		end 
 	end
 
 	get "/success" do
